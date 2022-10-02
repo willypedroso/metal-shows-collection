5b6b0ffa9820372156c26a8c7c9d06d3566f9d78
Will Pedroso <willyng.pedroso@gmail.com>
2022-10-02 20:22:43 -0300

    fix motorhead bug

--- src/pages/motorhead.jsx
+++ src/pages/motorhead.jsx
@@ -4,13 +4,15 @@ import Main from "../components/Main";
 import Menu from "../components/Menu";
 
 export default function Motorhead() {
-    <div>
-        <Header/>
-        <Menu/>
-        <Main title="Motörhead">
-            <Card title="Hammersmith - 1985">
-                <iframe className="rounded-md m-2" width="380" height="280" src="https://www.youtube.com/embed/GeXGW1yXgDg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-            </Card>
-        </Main>
-    </div>
+    return (
+        <div>
+            <Header/>
+            <Menu/>
+            <Main title="Motörhead">
+                <Card title="Hammersmith - 1985">
+                    <iframe className="rounded-md m-2" width="380" height="280" src="https://www.youtube.com/embed/GeXGW1yXgDg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                </Card>
+            </Main>
+        </div>
+    )
 }
