caa925ae246f5ae75cb8fcd76f10d201ba3f37b1
Will Pedroso <willyng.pedroso@gmail.com>
2022-10-03 20:27:31 -0300

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 export default function Footer() {
     return (
-        <p className="w-1/2 m-auto text-center mt-4">Develop by <a className="font-black hover:text-orange-600" href="https://instagram.com/relouord" target="_blank" rel="noreferrer noopener">RELOUORD</a> &copy;2022</p>
+        <p className="w-1/2 m-auto text-center mt-4">Develop by <a className="font-black hover:text-orange-600" href="https://instagram.com/relouord" target="_blank" rel="noreferrer noopener">RELOUORD</a> and <a className="font-black hover:text-orange-600" href="https://instagram.com/chapolinator_" target="_blank" rel="noreferrer noopener">CHAPOLINATOR</a> &copy;2022</p>
     )
 }
