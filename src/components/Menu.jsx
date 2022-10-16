4e2b24d729db14a5a329e44717b7b646cb56c4f0
Will Pedroso <willyng.pedroso@gmail.com>
2022-10-16 19:35:27 -0300

    add search to menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -6,10 +6,13 @@ import Link from "next/link";
 export default function Menu() {
     const [visibleMenu, setVisibleMenu] = useState(false);
     const [menu, setMenu] = useState("band");
+    const [textInput, setTextInput] = useState("");
 
     function visibleChange() {
-        setVisibleMenu(!visibleMenu)
+        setVisibleMenu(!visibleMenu);
     }
+
+    const filteredBands = bands.filter((band) => band.name.toLowerCase().startsWith(textInput.toLowerCase()));
 
     return (
         <nav className={visibleMenu ? "fixed top-0 left-0 h-[100vh] overflow-y-scroll overflow-x-clip min-w-[350px] w-1/5 bg-gray-900 text-white text-2xl" : "sticky text-center top-0 mb-[-70px] left-2 w-16"}>
@@ -17,13 +20,19 @@ export default function Menu() {
                 <li className="font-creeper
                         min-w-full mt-2 p-3
                         bg-orange-600 text-black cursor-pointer sticky top-0" onClick={visibleChange}>{!visibleMenu ? "Menu" : "Close"}</li>
-                        {visibleMenu ? 
-                        <select className="bg-black text-orange-600 text-[2rem] min-w-[300px] mt-3 font-creeper ml-3" onChange={e => setMenu(e.target.value)}>
-                            <option value="band">Bands</option>
-                            <option value="genre">Genres</option>
-                        </select>
-                        : false}
-                {visibleMenu && menu == "band" ? bands.map((band, i) => (
+                        
+                {visibleMenu ? 
+                    <select className="bg-black text-orange-600 text-[2rem] min-w-[300px] mt-3 font-creeper ml-3" onChange={e => setMenu(e.target.value)}>
+                        <option value="band">Bands</option>
+                        <option value="genre">Genres</option>
+                    </select>
+                    : false}
+                
+                {visibleMenu && menu == "band" ?
+                <input className="w-[250px] align-middle ml-[40px] mt-3 mb-1 text-black" placeholder="Search" type="text" value={textInput} onChange={e => setTextInput(e.target.value)}/>
+                : false}
+
+                {visibleMenu && menu == "band" ? filteredBands.map((band, i) => (
                     <Link key={i} href={band.path}><li className="font-creeper bg-black
                         min-w-full mt-2 p-3 cursor-pointer
                         hover:bg-orange-600 hover:text-black
