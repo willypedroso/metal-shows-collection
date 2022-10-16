d73a21025282facee75160ec47a41d1f2320cd82
Will Pedroso <willyng.pedroso@gmail.com>
2022-10-16 16:24:57 -0300

    add link to band titles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,17 @@
+import Link from "next/link";
+
 export default function Card(props) {
+
+    const bandPath = "/"+((props.titleBand).toLowerCase()).replace(" ","");
+
     return (
         <div className="w-[400px] min-h-[300px]
             rounded-lg bg-red-900
             flex flex-col justify-center align-middle m-2
             border-black border-2 shadow-lg shadow-black">
-            <h2 className="text-center mt-1 font-creeper text-2xl">{props.titleBand}</h2>
+            <Link href={bandPath}>
+                <h2 className="text-center mt-1 p-0 font-creeper text-2xl cursor-pointer hover:text-orange-500">{props.titleBand}</h2>
+            </Link>
             <h2 className="text-center mt-0 font-creeper text-2xl">{props.titleShow}</h2>
             <span className="self-center">
             <iframe className="rounded-md m-2" width="380" height="280" src={`https://www.youtube.com/embed/${props.url}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
