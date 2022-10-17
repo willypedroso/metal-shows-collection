59967a3136463ea93694a7668b094bedf660ee11
Will Pedroso <willyng.pedroso@gmail.com>
2022-10-17 05:41:45 -0300

    fix card title bands pages bug

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -13,11 +13,11 @@ export default function Card(props) {
             rounded-lg bg-red-900
             flex flex-col justify-center align-middle m-2
             border-black border-2 shadow-lg shadow-black">
-            <Link href={bandPath}>
+            {bandPath != "" ? <Link href={bandPath}>
                 <h2 className="text-center bg-black text-white block w-auto p-2 mt-[7px] rounded-md self-center
                  font-creeper text-2xl cursor-pointer hover:text-orange-500">{props.titleBand}</h2>
-            </Link>
-            <h2 className="text-center mt-0 font-creeper text-2xl">{props.titleShow}</h2>
+            </Link> : false}
+            <h2 className="text-center mt-1 mb-[-5px] font-creeper text-2xl">{props.titleShow}</h2>
             <span className="self-center">
             <iframe className="rounded-md m-2" width="380" height="280" src={`https://www.youtube.com/embed/${props.url}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </span>
