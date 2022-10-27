d73d08d6e6bb9077edf038e901f7978701f35bca
Will Pedroso <willyng.pedroso@gmail.com>
2022-10-27 05:16:03 -0300

    change color of header subtitle

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <div className="w-1/2 m-auto text-center mt-4 mb-4">
+        <div className="w-1/2 m-auto text-center mt-4 pb-4">
             <p>Develop by <a className="font-black hover:text-orange-600" href="https://instagram.com/relouord" target="_blank" rel="noreferrer noopener">RELOUORD</a> and <a className="font-black hover:text-orange-600" href="https://instagram.com/chapolinator_" target="_blank" rel="noreferrer noopener">CHAPOLINATOR</a> &copy;2022</p>
             <p>Contact us for sugestions and bug warnings</p>
         </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
             <Link href="/">
                 <h1 className="text-6xl  self-center p-2 cursor-pointer hover:text-orange-500">Metal Shows Collection</h1>
             </Link>
-            <h3 className="text-[1.1rem] text-purple-600 pb-2">The biggest Heavy Metal concerts collection of all times</h3>
+            <h3 className="text-[1.1rem] text-orange-600 pb-2">The biggest Heavy Metal concerts collection of all times</h3>
         </header>
     )
 }
