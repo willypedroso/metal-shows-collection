623283b5f68043c206403dd5c3b72b3f60fba03b
Will Pedroso <willyng.pedroso@gmail.com>
2022-10-30 08:18:02 -0300

    add hidden message to visitors on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,9 @@ export default function Footer() {
         <div className="w-1/2 m-auto text-center mt-4 pb-4">
             <p>Develop by <a className="font-black hover:text-orange-600" href="https://instagram.com/relouord" target="_blank" rel="noreferrer noopener">RELOUORD</a> and <a className="font-black hover:text-orange-600" href="https://instagram.com/chapolinator_" target="_blank" rel="noreferrer noopener">CHAPOLINATOR</a> &copy;2022</p>
             <p>Contact us for sugestions and bug warnings</p>
+
+            {/* Hidden message to visitors */}
+            <h1 className="mt-3"><strong>Happy Halloween!</strong></h1>
         </div>
     )
 }
